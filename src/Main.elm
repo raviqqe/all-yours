88a282210274fcfbf02f55e7e6b6e63835bc72f8
Yota Toyama <raviqqe@gmail.com>
2017-02-25 20:10:37 +0900

    Add start screen

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,7 @@ import Html.Attributes exposing (..)
 import Keyboard exposing (..)
 import List exposing (..)
 import Random exposing (..)
+import Text
 import Time exposing (..)
 
 import Camera exposing (Camera)
@@ -24,40 +25,47 @@ skyColor = rgb 128 128 255
 
 -- Model
 
+type Screen = Start | Play
+
 type alias Game =
   { human : Human
   , enemies : List Enemy
   , earth : Earth
   , camera : Camera
   , lastEnemyTime : Time
+  , screen : Screen
   }
 
 
 -- Update
 
 update : Msg -> Game -> (Game, Cmd Msg)
-update msg model =
-  let
-    newEnemies = case msg of
-      GenEnemy _ -> Enemy.initial :: model.enemies
-      _ -> model.enemies
+update msg model = case model.screen of
+  Play ->
+    let
+      newEnemies = case msg of
+        GenEnemy _ -> Enemy.initial :: model.enemies
+        _ -> model.enemies
 
-    generateEnemy =
-      case msg of
-        Clock t -> Random.generate
-          (\i -> if t - model.lastEnemyTime > 500 && i % 3000 == 0 then GenEnemy t else NoOp)
-          (Random.int minInt maxInt)
-        _ -> Cmd.none
-  in
-    { model |
-        human = Human.update msg model.earth model.human,
-        enemies = List.map (Enemy.update msg model.earth) newEnemies,
-        lastEnemyTime = case msg of
-          GenEnemy t -> t
-          _ -> model.lastEnemyTime
-    } ! [
-      generateEnemy
-    ]
+      generateEnemy =
+        case msg of
+          Clock t -> Random.generate
+            (\i -> if t - model.lastEnemyTime > 500 && i % 3000 == 0 then GenEnemy t else NoOp)
+            (Random.int minInt maxInt)
+          _ -> Cmd.none
+    in
+      { model |
+          human = Human.update msg model.earth model.human,
+          enemies = List.map (Enemy.update msg model.earth) newEnemies,
+          lastEnemyTime = case msg of
+            GenEnemy t -> t
+            _ -> model.lastEnemyTime
+      } ! [
+        generateEnemy
+      ]
+  Start -> case msg of
+    Msg.Jump -> { model | screen = Play } ! []
+    _ -> model ! []
 
 
 -- View
@@ -76,7 +84,9 @@ view model =
             filled skyColor
         , Earth.view model.earth model.camera
         , Human.view model.human
-        ] |> flip append (List.map Enemy.view model.enemies))
+        ] |>
+        flip append (List.map Enemy.view model.enemies) |>
+        flip append (if model.screen == Start then [toForm (centered (Text.fromString "All Yours"))] else []))
     ]
   ]
 
@@ -85,14 +95,21 @@ view model =
 
 main : Program Never Game Msg
 main = program
-  { init = Game Human.initial [] Earth.initial Camera.initial 0 ! []
+  { init =
+      { human = Human.initial
+      , enemies = []
+      , earth = Earth.initial
+      , camera = Camera.initial
+      , lastEnemyTime = 0
+      , screen = Start
+      } ! []
   , update = update
   , view = view
   , subscriptions = \_ ->
       Sub.batch
         [ Keyboard.downs (\code ->
             case code of
-              32 -> Jump
+              32 -> Msg.Jump
               _ -> NoOp
             )
         , AnimationFrame.diffs (inSeconds >> Delta)
